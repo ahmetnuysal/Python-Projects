eef976c7f85c853b248162ed8221e0427222a0fa
Ahmet N. Uysal <85068070+ahmetnuysal@users.noreply.github.com>
2022-08-14 12:06:24 +0300

    Add files via upload

---
+++ Python Spor Salonu Üyelik Sorgulama.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+üyesayisi=int(input("Yeni Üye Sayısını Giriniz: "))
+isim=[]
+tarih=[]
+sayi=0
+while True:
+
+    if üyesayisi==sayi:
+        break
+    else:
+        isim.append(input(str(sayi+1)+"." +"üye ismini giriniz: "))
+        tarih.append(input(str(sayi+1)+"."+ " " +"üyenin son üyelik tarihini giriniz: "))
+    sayi=1+sayi
+
+istenilen=(input("üye isimini giriniz: "))
+for i in isim:
+    if istenilen==i:
+        istenilenindex=isim.index(istenilen)
+        print(tarih[istenilenindex])
+        break
+    else:
+        print("Üye Bulunamadı")
+  
+
+
+# In[ ]:
+
+
+
+
